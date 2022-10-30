9092e3aced37387e20250acf9cb7a5aae57423f1
Len Lutz <programmer@slment.com>
2022-10-30 14:41:42 -0600

    Rename getPosts to DisplayPosts and fix comments

--- callbacks.js
+++ callbacks.js
@@ -5,8 +5,8 @@ const posts = [
 ]
 
 // This function reads all the posts currently in the array and adds each one
-// to the web page as a list item
-function getPosts() {
+// to the web page in a table
+function displayPosts() {
     // initialize the list (table)
     let output = '<h1>Blog Posts</h1><div><table><tr><th style="width: 35%">Title</th><th>Text</th></tr>';
     // loop through the array of posts and add each of them to the list to be displayed
@@ -18,8 +18,8 @@ function getPosts() {
     document.body.innerHTML = output;
 }
 
-// This functions allows us to add additianal posts to the original array and
-// then it calls the  'callback' function (getPosts) to actually send the list 
+// This function allows us to add additianal posts to the original array and
+// then it calls the  'callback' function (displayPosts) to actually resend the list 
 // to the web page
 function createPost(post, callback) {
     posts.push(post);
@@ -27,15 +27,15 @@ function createPost(post, callback) {
 }
 
 // The following lines call the function that adds new posts to the array and includes the
-// name of the function (the callbak function, getPosts) that sends the list to the web page.
+// name of the function (the callbak function, displayPosts) that sends the list to the web page.
 // The first one does it immediately.  The remaining lines are delayed by 1, 3, and 2
 // seconds.  The delay determines the order that each post will be added to the list, which,
 //  in this case, because the delay on the line that adds post five waits longer that the delay 
 // for post six, should display the sixth post before the fifth.
-createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
-setTimeout(() => { createPost({ title: 'Post Four', body: 'This is post four' }, getPosts) }, 1000);
-setTimeout(() => { createPost({ title: 'Post Five', body: 'This is post five' }, getPosts) }, 3000);
+createPost({ title: 'Post Three', body: 'This is post three' }, displayPosts);
+setTimeout(() => { createPost({ title: 'Post Four', body: 'This is post four' }, displayPosts) }, 1000);
+setTimeout(() => { createPost({ title: 'Post Five', body: 'This is post five' }, displayPosts) }, 3000);
 setTimeout(() => { createPost({ title: 'Post Six', body: 'This is post six, which has ' +
     'shorter timeout than post five, so it should actually show up BEFORE post five. ' +
     'It is also a much longer post so it should cause the text to wrap and show what a post ' +
-    'will look like when it is too long to fit on a single line.' }, getPosts) }, 2000);
+    'will look like when it is too long to fit on a single line.' }, displayPosts) }, 2000);
