b4750af96156122902cd5359ee9af783e7e0b820
Len Lutz <programmer@slment.com>
2022-10-30 14:07:30 -0600

    Changed list to a table, added stylesheet

--- callbacks.js
+++ callbacks.js
@@ -7,12 +7,13 @@ const posts = [
 // This function reads all the posts currently in the array and adds each one
 // to the web page as a list item
 function getPosts() {
-    // initialize the list
-    let output = '';
+    // initialize the list (table)
+    let output = '<h1>Blog Posts</h1><div><table><tr><th style="width: 35%">Title</th><th>Text</th></tr>';
     // loop through the array of posts and add each of them to the list to be displayed
     posts.forEach((post, index) => {
-        output += `<li>${post.title}: &nbsp;&nbsp;&nbsp;${post.body}</li>`;
+        output += `<tr><td>${post.title}<td>${post.body}</td></tr>`;
     });
+    output += '</table></div>'
     // send the list to the web page
     document.body.innerHTML = output;
 }
@@ -30,8 +31,11 @@ function createPost(post, callback) {
 // The first one does it immediately.  The remaining lines are delayed by 1, 3, and 2
 // seconds.  The delay determines the order that each post will be added to the list, which,
 //  in this case, because the delay on the line that adds post five waits longer that the delay 
-// for the line that adds post six, should display the sixth post before the fifth.
+// for post six, should display the sixth post before the fifth.
 createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
 setTimeout(() => { createPost({ title: 'Post Four', body: 'This is post four' }, getPosts) }, 1000);
 setTimeout(() => { createPost({ title: 'Post Five', body: 'This is post five' }, getPosts) }, 3000);
-setTimeout(() => { createPost({ title: 'Post Six', body: 'This is post six' }, getPosts) }, 2000);
+setTimeout(() => { createPost({ title: 'Post Six', body: 'This is post six, which has ' +
+    'shorter timeout than post five, so it should actually show up BEFORE post five. ' +
+    'It is also a much longer post so it should cause the text to wrap and show what a post ' +
+    'will look like when it is too long to fit on a single line.' }, getPosts) }, 2000);
